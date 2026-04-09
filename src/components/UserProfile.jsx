de1912d1ae7ae78463e4dd23c0f1b10fa9d5bd7b
Harsh Dhiman <harsh61355@gmail.com>
2026-04-09 21:45:25 +0530

    Created structure for user profile and added elements into sidebar menu component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,6 +1,12 @@
 const UserProfile = () => {
     return (
-        <div className="p-4 border">UserProfile</div>
+        <div className="p-4 border flex gap-2 items-center">
+            <span>icon</span>
+            <div className="flex flex-col">
+                <span className="font-semibold">User Name</span>
+                <span className="text-xs">Logout</span>
+            </div>
+        </div>
     )
 }
 
