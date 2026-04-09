fb8df94905a545eb8df6729e57aa21818f900b8d
Harsh Dhiman <harsh61355@gmail.com>
2026-04-09 20:32:44 +0530

    Added scroll to bottom automatically functionality into messageContainer

--- src/components/ChatWindowMessages.jsx
+++ src/components/ChatWindowMessages.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react"
 import MyMessage from "./MyMessage.jsx"
 import OthersMessage from "./OthersMessage.jsx"
 
-const ChatWindowMessages = () => {
+const ChatWindowMessages = ({ ref }) => {
+    useEffect(() => {
+        ref.current.scrollIntoView({ behavior: "smooth" })
+    }, [ref])
     return (
-        <div className="flex-1! border flex flex-col gap-2 p-4 overflow-y-auto">
+        <div className="flex-1! border flex flex-col gap-2 p-4 overflow-y-auto no-scrollbar">
             <OthersMessage />
             <MyMessage />
             <MyMessage />
@@ -13,6 +17,7 @@ const ChatWindowMessages = () => {
             <OthersMessage />
             <OthersMessage />
             <MyMessage />
+            <div ref={ref} className="h-0"></div>
         </div>
     )
 }
--- src/sections/ChatWindow.jsx
+++ src/sections/ChatWindow.jsx
@@ -1,12 +1,14 @@
 import ChatWindowHeader from "../components/ChatWindowHeader.jsx"
 import ChatWindowMessages from "../components/ChatWindowMessages.jsx"
 import ChatWindowInputMessage from "../components/ChatWindowInputMessage.jsx"
+import { useRef } from "react"
 
 const ChatWindow = () => {
+    const ref = useRef()
     return (
         <div className="flex-1! border flex flex-col justify-between overflow-y-hidden">
             <ChatWindowHeader />
-            <ChatWindowMessages />
+            <ChatWindowMessages ref={ref} />
             <ChatWindowInputMessage />
         </div>
     )
