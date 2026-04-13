a1f1e5161ee609aca9c057c6a01ea04f56e4d9e9
Harsh Dhiman <harsh61355@gmail.com>
2026-04-13 18:05:52 +0530

    Added colors for login and signup pages based on theme light and dark

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,39 @@
+import { useContext } from "react"
+import { ThemeContext } from "../context/ThemeSwitcher"
 import { Link } from "react-router-dom"
 
 const Login = () => {
+    const { theme } = useContext(ThemeContext)
     return (
-        <div className="min-h-dvh flex items-center justify-center">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div className={`min-h-dvh flex items-center justify-center ${theme == "light" ? 'bg-slate-100 text-slate-900' : 'bg-slate-900 text-slate-50'}`}>
+            <div className={`w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ${theme == 'light' ? 'bg-slate-50' : 'bg-slate-950'}`}>
+                <div className={`p-6 space-y-4 md:space-y-6 sm:p-8`}>
+                    <h1 className={`text-xl font-bold leading-tight tracking-tight md:text-2xl`}>
                         Sign in to your account
                     </h1>
-                    <form className="space-y-4 md:space-y-6" action="#">
+                    <form className={`space-y-4 md:space-y-6" action="#`}>
                         <div>
-                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                            <label for="email" className={`block mb-2 text-sm font-medium`}>Your email</label>
+                            <input type="email" name="email" id="email" className={`rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} placeholder="name@domain.com" required="" />
                         </div>
                         <div>
-                            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            <label for="password" className={`block mb-2 text-sm font-medium`}>Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className={`rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} required="" />
                         </div>
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-start">
-                                <div className="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                        <div className={`flex items-center justify-between`}>
+                            <div className={`flex items-start`}>
+                                <div className={`flex items-center h-5`}>
+                                    <input id="remember" aria-describedby="remember" type="checkbox" className={`w-4 h-4 rounded focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} required="" />
                                 </div>
-                                <div className="ml-3 text-sm">
-                                    <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                <div className={`ml-3 text-sm`}>
+                                    <label for="remember" className={``}>Remember me</label>
                                 </div>
                             </div>
-                            <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                            <a href="#" className={`text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 text-sky-600`}>Forgot password?</a>
                         </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <Link to="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                        <button type="submit" className={`w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-primary-700 dark:focus:ring-primary-800 active:scale-95 bg-sky-600 text-white`}>Sign in</button>
+                        <p className={`text-sm font-light`}>
+                            Don’t have an account yet? <Link to="/signup" className={`font-medium text-primary-600 hover:underline dark:text-primary-500 text-sky-600`}>Sign up</Link>
                         </p>
                     </form>
                 </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,37 +1,40 @@
+import { useContext } from "react"
+import { ThemeContext } from "../context/ThemeSwitcher"
 import { Link } from "react-router-dom"
 
 const Signup = () => {
+    const { theme } = useContext(ThemeContext)
     return (
-        <div className="min-h-dvh flex items-center justify-center">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div className={`min-h-dvh flex items-center justify-center ${theme == "light" ? 'bg-slate-100 text-slate-900' : 'bg-slate-900 text-slate-50'}`}>
+            <div className={`w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ${theme == 'light' ? 'bg-slate-50' : 'bg-slate-950'}`}>
+                <div className={`p-6 space-y-4 md:space-y-6 sm:p-8`}>
+                    <h1 className={`text-xl font-bold leading-tight tracking-tight md:text-2xl`}>
                         Create an account
                     </h1>
-                    <form className="space-y-4 md:space-y-6" action="#">
+                    <form className={`space-y-4 md:space-y-6" action="#`}>
                         <div>
-                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                            <label htmlFor="email" className={`block mb-2 text-sm font-medium`}>Your email</label>
+                            <input type="email" name="email" id="email" className={`text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} placeholder="name@domain.com" required="" />
                         </div>
                         <div>
-                            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            <label htmlFor="password" className={`block mb-2 text-sm font-medium`}>Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className={`text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} required="" />
                         </div>
                         <div>
-                            <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            <label htmlFor="confirm-password" className={`block mb-2 text-sm font-medium`}>Confirm password</label>
+                            <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className={`text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} required="" />
                         </div>
-                        <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                                <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                        <div className={`flex items-start`}>
+                            <div className={`flex items-center h-5`}>
+                                <input id="terms" aria-describedby="terms" type="checkbox" className={`w-4 h-4 rounded focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 border ${theme == "light" ? 'bg-slate-100 border-slate-200' : 'bg-slate-900 border-slate-800'}`} required="" />
                             </div>
-                            <div className="ml-3 text-sm">
-                                <label for="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                            <div className={`ml-3 text-sm`}>
+                                <label htmlFor="terms" className={`font-light`}>I accept the <a className={`font-medium text-primary-600 hover:underline dark:text-primary-500 text-sky-600`} href="#">Terms and Conditions</a></label>
                             </div>
                         </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Already have an account? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                        <button type="submit" className={`w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-sky-600 text-white active:scale-95`}>Create an account</button>
+                        <p className={`text-sm font-light`}>
+                            Already have an account? <Link to="/login" className={`font-medium text-primary-600 hover:underline dark:text-primary-500 text-sky-600`}>Login here</Link>
                         </p>
                     </form>
                 </div>
