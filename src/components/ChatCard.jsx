2e01d4ceb237ce6785b8bfc7a59aed548a2062f3
Harsh Dhiman <harsh61355@gmail.com>
2026-04-19 12:51:16 +0530

    Added real users in Chat section fetched from db

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -1,17 +1,18 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeSwitcher.jsx'
 
-const ChatCard = () => {
+const ChatCard = ({ displayName, lastMessage="Hello Harsh!" }) => {
     const { theme } = useContext(ThemeContext)
     return (
+        // <ChatCard key={chat[0]} displayName={chat[1].userInfo.displayName} lastMessage={chat[1].userInfo.lastMessage?.text}/>
         <div className={`flex gap-4 items-center p-1 rounded-lg cursor-pointer ${theme == "light" ? 'hover:bg-slate-100' : 'hover:bg-slate-900'}`}>
             <span className="size-9 border rounded-lg"></span>
             <div className="flex-1! flex flex-col">
                 <div className="flex justify-between items-center">
-                    <span className="font-semibold">Odama Studio</span>
+                    <span className="font-semibold">{displayName}</span>
                     <span className={`text-xs ${theme == "light" ? 'text-slate-600' : 'text-slate-300'}`}>08:08 PM</span>
                 </div>
-                <div className={`text-xs ${theme == "light" ? 'text-slate-600' : 'text-slate-300'}`}>Thanks! Tokyo</div>
+                <div className={`text-xs ${theme == "light" ? 'text-slate-600' : 'text-slate-300'}`}>{lastMessage}</div>
             </div>
         </div>
     )
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,20 +1,30 @@
+import { useContext, useEffect, useState } from 'react'
 import ChatCard from './ChatCard.jsx'
+import { onSnapshot, doc } from 'firebase/firestore'
+import { AuthContext } from '../context/AuthContextProvider.jsx'
+import { db } from '../firebase.js'
 
 const Chats = () => {
+    const { currentUser } = useContext(AuthContext)
+    const [chats, setChats] = useState([])
+
+    useEffect(() => {
+        const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+            setChats(doc.data())
+        })
+
+        return () => {
+            unsub()
+        }
+    }, [currentUser.uid])
+
+    console.log(chats)
+    console.log(Object.entries(chats))
     return (
         <div className="flex-1! flex flex-col gap-1 px-2 py-4 overflow-y-auto no-scrollbar">
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
+            {Object.entries(chats)?.map((chat) => (
+                <ChatCard key={chat[0]} displayName={chat[1].userInfo.displayName} lastMessage={chat[1].userInfo.lastMessage?.text} />
+            ))}
         </div>
     )
 }
